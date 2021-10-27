f5601b50cd39eb2e7b3176ac346cf2cdf11b3aac
Chinmay <sahoochinmay54@gmail.com>
2021-10-27 10:28:49 +0530

    lecture 5 completed

---
+++ lecture 5/P4.java
@@ -0,0 +1,14 @@
+public class P4{
+    public static void main(String args[])
+    {
+        int n = 4;
+        for(int i=4;i>0;i--)
+        {
+            for(int j =1 ;j <=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
